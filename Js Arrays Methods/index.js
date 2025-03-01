b223f6064069d7022a06ff7054ae364cf062e27a
Sohail Shoukat <sohailshoukat131@gmail.com>
2025-03-01 09:41:32 +0500

    Js Array methods

---
+++ Js Arrays Methods/index.js
@@ -0,0 +1,33 @@
+console.log("Hello Js Methods");
+
+// Arrays methods
+
+// push()
+const fruits = ["Apple", "Banana", "Orange", "Mango"];
+console.log(fruits);
+
+fruits.push("Kiwi");
+console.log(fruits);
+
+// shift()
+const fruits2 = ["Apple", "Banana", "Orange", "Mango"];
+console.log(fruits2);
+fruits2.shift();
+fruits2.shift();
+console.log(fruits2);
+
+// unshift()
+
+const fruits3 = ["Apple", "Banana", "Orange", "Mango"];
+console.log(fruits3);
+fruits3.unshift("Kiwi");
+console.log(fruits3);
+
+// pop()
+
+const fruits4 = ["Apple", "Banana", "Orange", "Mango"];
+console.log(fruits4);
+fruits4.pop();
+console.log(fruits4);
+
+
